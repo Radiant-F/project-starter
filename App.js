297afcd2792eb77d4bf304e494ebea1ee95d1e5a
RadiantF <radiantfadilah0@gmail.com>
2021-03-22 10:56:59 +0700

    add redux

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
 import Authorization from './src/components/Authorization';
+import Demo from './src/Demo';
+import {Provider} from 'react-redux';
+import {store} from './src/redux/Store';
 
 export default class App extends Component {
   render() {
-    return <Authorization />;
+    return (
+      <Provider store={store}>
+        <Authorization />
+      </Provider>
+    );
   }
 }
