b2c5cd09e63d0626d8621d36ddd018ccdfec6cbd
RadiantF-pondok <radiantfadilah0@gmail.com>
2021-03-27 08:58:45 +0700

    update styling

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,11 +1,30 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, TextInput, View} from 'react-native';
+import {styles} from '../components/styles';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+    };
+  }
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style={{flex: 1}}>
+        <View style={{...styles.header, padding: -10, paddingHorizontal: 10}}>
+          <TextInput
+            placeholder="cari sesuatu..."
+            onChangeText={input => this.setState({search: input})}
+            onEndEditing={() => console.log(this.state.search)}
+            selectTextOnFocus
+            blurOnSubmit
+            autoFocus
+          />
+        </View>
+        <View style={{padding: 10}}>
+          <Text> Pencarian untuk: {this.state.search} </Text>
+        </View>
       </View>
     );
   }
