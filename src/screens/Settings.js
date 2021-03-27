0232fbae7273e1f5713c26d135dd33d3d3926d00
RadiantF-pondok <radiantfadilah0@gmail.com>
2021-03-27 08:59:16 +0700

    update component

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, TouchableNativeFeedback, View} from 'react-native';
+import {ScrollView, Text, TouchableNativeFeedback, View} from 'react-native';
 import {styles} from '../components/styles';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
@@ -11,82 +11,96 @@ export default class Settings extends Component {
           <View style={{flexDirection: 'row', alignItems: 'center'}}>
             <TouchableNativeFeedback
               onPress={() => this.props.navigation.goBack()}>
-              <Icon name="chevron-left" size={35} />
+              <Icon name="chevron-left" size={30} />
             </TouchableNativeFeedback>
             <Text style={styles.titleHeader}>Pengaturan & Konfigurasi</Text>
           </View>
         </View>
-        <View style={{flex: 1, backgroundColor: 'white'}}>
-          <TouchableNativeFeedback>
-            <View style={styles.viewOption}>
-              <Icon name="crown-outline" size={30} style={styles.iconOption} />
-              <Text> Pugasan </Text>
+        <ScrollView>
+          <View style={{flex: 1, backgroundColor: 'white'}}>
+            <TouchableNativeFeedback>
+              <View style={styles.viewOption}>
+                <Icon
+                  name="crown-outline"
+                  size={30}
+                  style={styles.iconOption}
+                />
+                <Text> Pugasan </Text>
+              </View>
+            </TouchableNativeFeedback>
+            <TouchableNativeFeedback>
+              <View style={styles.viewOption}>
+                <Icon
+                  name="palette-outline"
+                  size={30}
+                  style={styles.iconOption}
+                />
+                <Text> Tema </Text>
+              </View>
+            </TouchableNativeFeedback>
+            <TouchableNativeFeedback>
+              <View style={styles.viewOption}>
+                <Icon name="translate" size={30} style={styles.iconOption} />
+                <Text> Bahasa </Text>
+              </View>
+            </TouchableNativeFeedback>
+            <TouchableNativeFeedback>
+              <View style={styles.viewOption}>
+                <Icon
+                  name="calendar-clock"
+                  size={30}
+                  style={styles.iconOption}
+                />
+                <Text> Tanggal dan Waktu </Text>
+              </View>
+            </TouchableNativeFeedback>
+            <TouchableNativeFeedback>
+              <View style={styles.viewOption}>
+                <Icon
+                  name="calendar-check-outline"
+                  size={30}
+                  style={styles.iconOption}
+                />
+                <Text> Pengaturan Tugas </Text>
+              </View>
+            </TouchableNativeFeedback>
+            <TouchableNativeFeedback>
+              <View style={styles.viewOption}>
+                <Icon
+                  name="bell-ring-outline"
+                  size={30}
+                  style={styles.iconOption}
+                />
+                <Text> Opsi Notifikasi </Text>
+              </View>
+            </TouchableNativeFeedback>
+            <TouchableNativeFeedback>
+              <View style={styles.viewOption}>
+                <Icon
+                  name="cloud-upload-outline"
+                  size={30}
+                  style={styles.iconOption}
+                />
+                <Text> Penyimpanan Awan </Text>
+              </View>
+            </TouchableNativeFeedback>
+            <TouchableNativeFeedback>
+              <View style={styles.viewOption}>
+                <Icon
+                  name="database-edit"
+                  size={30}
+                  style={styles.iconOption}
+                />
+                <Text> Manajemen Data </Text>
+              </View>
+            </TouchableNativeFeedback>
+            <View style={{alignSelf: 'center', margin: 10}}>
+              <Text style={{color: 'grey'}}>
+                siFreelancer for Android v0.8.0-dev-build
+              </Text>
             </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback>
-            <View style={styles.viewOption}>
-              <Icon
-                name="palette-outline"
-                size={30}
-                style={styles.iconOption}
-              />
-              <Text> Tema </Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback>
-            <View style={styles.viewOption}>
-              <Icon name="translate" size={30} style={styles.iconOption} />
-              <Text> Bahasa </Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback>
-            <View style={styles.viewOption}>
-              <Icon name="calendar-clock" size={30} style={styles.iconOption} />
-              <Text> Tanggal dan Waktu </Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback>
-            <View style={styles.viewOption}>
-              <Icon
-                name="calendar-check-outline"
-                size={30}
-                style={styles.iconOption}
-              />
-              <Text> Pengaturan Tugas </Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback>
-            <View style={styles.viewOption}>
-              <Icon
-                name="bell-ring-outline"
-                size={30}
-                style={styles.iconOption}
-              />
-              <Text> Opsi Notifikasi </Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback>
-            <View style={styles.viewOption}>
-              <Icon
-                name="cloud-upload-outline"
-                size={30}
-                style={styles.iconOption}
-              />
-              <Text> Penyimpanan Awan </Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback>
-            <View style={styles.viewOption}>
-              <Icon name="database-edit" size={30} style={styles.iconOption} />
-              <Text> Manajemen Data </Text>
-            </View>
-          </TouchableNativeFeedback>
-          <View style={{alignSelf: 'center', margin: 10}}>
-            <Text style={{color: 'grey'}}>
-              siFreelancer for Android v0.8.0-dev-build
-            </Text>
           </View>
-        </View>
+        </ScrollView>
       </View>
     );
   }
