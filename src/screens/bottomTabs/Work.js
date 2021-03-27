ba82c8f7c63f380afb7abcdff9440e29baa4baae
RadiantF-pondok <radiantfadilah0@gmail.com>
2021-03-27 09:00:59 +0700

    update component

--- src/screens/bottomTabs/Work.js
+++ src/screens/bottomTabs/Work.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, TouchableNativeFeedback, View} from 'react-native';
+import {ScrollView, Text, TouchableNativeFeedback, View} from 'react-native';
 import {styles} from '../../components/styles';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
@@ -10,71 +10,81 @@ export default class Work extends Component {
         <View style={{...styles.header, height: 55}}>
           <Text style={styles.titleHeader}>Kerjamu</Text>
         </View>
-        <View style={{flex: 1, backgroundColor: 'white'}}>
-          <TouchableNativeFeedback>
-            <View style={styles.viewOption}>
-              <Icon name="account-circle" size={30} style={styles.iconOption} />
-              <Text> Akun </Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback>
-            <View style={styles.viewOption}>
-              <Icon name="crown-outline" size={30} style={styles.iconOption} />
-              <Text> Pugasan </Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback>
-            <View style={styles.viewOption}>
-              <Icon name="repeat" size={30} style={styles.iconOption} />
-              <Text> Tugas Rutin </Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback>
-            <View style={styles.viewOption}>
-              <Icon name="reminder" size={30} style={styles.iconOption} />
-              <Text> Pengingat </Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback>
-            <View style={styles.viewOption}>
-              <Icon name="note-outline" size={30} style={styles.iconOption} />
-              <Text> Catatan </Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback>
-            <View style={styles.viewOption}>
-              <Icon name="tag-outline" size={30} style={styles.iconOption} />
-              <Text> Tag </Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback>
-            <View style={styles.viewOption}>
-              <Icon
-                name="bookmark-multiple-outline"
-                size={30}
-                style={styles.iconOption}
-              />
-              <Text> Templat </Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback>
-            <View style={styles.viewOption}>
-              <Icon
-                name="calendar-multiselect"
-                size={30}
-                style={styles.iconOption}
-              />
-              <Text> Acara Kalender </Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback
-            onPress={() => this.props.navigation.navigate('Pengaturan')}>
-            <View style={styles.viewOption}>
-              <Icon name="cog" size={30} style={styles.iconOption} />
-              <Text> Pengaturan </Text>
-            </View>
-          </TouchableNativeFeedback>
-        </View>
+        <ScrollView>
+          <View style={{flex: 1, backgroundColor: 'white'}}>
+            <TouchableNativeFeedback>
+              <View style={styles.viewOption}>
+                <Icon
+                  name="account-circle"
+                  size={30}
+                  style={styles.iconOption}
+                />
+                <Text> Akun </Text>
+              </View>
+            </TouchableNativeFeedback>
+            <TouchableNativeFeedback>
+              <View style={styles.viewOption}>
+                <Icon
+                  name="crown-outline"
+                  size={30}
+                  style={styles.iconOption}
+                />
+                <Text> Pugasan </Text>
+              </View>
+            </TouchableNativeFeedback>
+            <TouchableNativeFeedback>
+              <View style={styles.viewOption}>
+                <Icon name="repeat" size={30} style={styles.iconOption} />
+                <Text> Tugas Rutin </Text>
+              </View>
+            </TouchableNativeFeedback>
+            <TouchableNativeFeedback>
+              <View style={styles.viewOption}>
+                <Icon name="reminder" size={30} style={styles.iconOption} />
+                <Text> Pengingat </Text>
+              </View>
+            </TouchableNativeFeedback>
+            <TouchableNativeFeedback>
+              <View style={styles.viewOption}>
+                <Icon name="note-outline" size={30} style={styles.iconOption} />
+                <Text> Catatan </Text>
+              </View>
+            </TouchableNativeFeedback>
+            <TouchableNativeFeedback>
+              <View style={styles.viewOption}>
+                <Icon name="tag-outline" size={30} style={styles.iconOption} />
+                <Text> Tag </Text>
+              </View>
+            </TouchableNativeFeedback>
+            <TouchableNativeFeedback>
+              <View style={styles.viewOption}>
+                <Icon
+                  name="bookmark-multiple-outline"
+                  size={30}
+                  style={styles.iconOption}
+                />
+                <Text> Templat </Text>
+              </View>
+            </TouchableNativeFeedback>
+            <TouchableNativeFeedback>
+              <View style={styles.viewOption}>
+                <Icon
+                  name="calendar-multiselect"
+                  size={30}
+                  style={styles.iconOption}
+                />
+                <Text> Acara Kalender </Text>
+              </View>
+            </TouchableNativeFeedback>
+            <TouchableNativeFeedback
+              onPress={() => this.props.navigation.navigate('Pengaturan')}>
+              <View style={styles.viewOption}>
+                <Icon name="cog" size={30} style={styles.iconOption} />
+                <Text> Pengaturan </Text>
+              </View>
+            </TouchableNativeFeedback>
+          </View>
+        </ScrollView>
       </View>
     );
   }
